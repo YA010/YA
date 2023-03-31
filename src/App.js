6e97998b19d387798a9fe51bb039128afdb9d4b4
careerscamp <isa@careers.camp>
2023-03-31 18:09:41 +0100

    finished ui for the mvp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+// Import required modules and components
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { IonApp, IonPage, IonContent, IonHeader, IonMenu, IonSplitPane, IonTitle, IonToolbar, IonIcon,  IonRouterOutlet,  IonTabs, IonImg, IonRoute, IonTabBar, IonTabButton, IonLabel, IonGrid, IonRow, IonCol, IonCard, IonBackButton, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonInput, IonButton } from '@ionic/react';
+import { calendar, personCircle, informationCircle, map, desktop, searchCircle } from 'ionicons/icons';
+import { createRoot } from 'react-dom/client';
+import { IonReactRouter } from '@ionic/react-router';
+
+// Import components to render 
+
+// Import CSS files
+import '@ionic/react/css/core.css';
+import './index.css';
 import './App.css';
+import { TextField } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <IonGrid >
+       
+    <IonRow>
+      <IonCol sizeXs="12" style={{marginTop: "10vh"}}>
+    
+<IonCardTitle id="cardstit" class='card-text' style={{textAlign: "center", fontWeight: "800"}}>Generate a custom cv </IonCardTitle>
+
+      </IonCol>
+      </IonRow>
+      <IonRow>
+          <IonCol sizeXs="12" >
+         
+          <IonCard  mode="ios" color="secondary" id="showcard" >
+        
+     <IonCardHeader >
+   
+ <IonCardTitle class='card-text'>Paste in your cv here </IonCardTitle>
+
+</IonCardHeader>
+     <IonCardContent>
+    
+     <TextField
+          id="standard-textarea"
+          label="Cv Here"
+          placeholder="finish"
+          multiline
+          focused
+          color='primary'
+          style={{width: "100%", color:"white"}}
+          variant="standard"
+     
+        />
+        <IonRow >
+        <IonCol sizeXs="6" >
+       <IonButton color="medium" expand="block">Preview</IonButton>
+       </IonCol>
+       <IonCol sizeXs="6" >
+       <IonButton color="medium" expand="block">Submit</IonButton>
+       </IonCol>
+       </IonRow>
+                  </IonCardContent>
+                  
+   </IonCard>
+          </IonCol>
+          </IonRow>
+  </IonGrid>
+ 
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import required modules and components
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React,{ Suspense } from 'react';
+import './index.css';
+import { IonApp, IonContent, IonHeader, IonMenu, IonSplitPane, IonTitle, IonToolbar, IonIcon,  IonRouterOutlet,  IonTabs, IonImg, IonRoute, IonTabBar, IonTabButton, IonPage, IonLabel, IonButton } from '@ionic/react';
+import { calendar, personCircle, informationCircle } from 'ionicons/icons';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import {  map, desktop,searchCircle } from 'ionicons/icons';
+import { createRoot } from 'react-dom/client';
+import { IonReactRouter } from '@ionic/react-router';
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import App from './App';
+import { Grid } from '@mui/material';
+
+
+
+// custom function to enable dark theme replace if you think yours is better
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+toggleDarkTheme(prefersDark.matches);
+
+
+// Listen for changes to the prefers-color-scheme media query
+prefersDark.addListener((mediaQuery) => toggleDarkTheme(mediaQuery.matches));
+
+// Add or remove the "dark" class based on if the media query matches
+function toggleDarkTheme(shouldAdd) {
+  document.body.classList.toggle('dark', shouldAdd);
+ 
+}
+
+
+// main function being rendered
+function RenderApp() {
+ 
+ return (
+  <IonReactRouter>
+  <IonApp>
+    <IonPage id="main">
+      <IonHeader fixed mode="ios">
+        <Grid id="contents" container spacing={0}>
+          <IonToolbar mode="ios" color="primary">
+            <Grid xs={5} sm={4} md={3} lg={3}>
+              <img id="logo" fill="#p4420" slot="start" src={require('./invoest.png')} />
+            </Grid>
+            {document.body.classList.contains("dark") ? (
+              <>
+                <IonButton mode="ios" color="medium" slot="end" onClick={() => toggleDarkTheme()}>
+                  <DarkModeRoundedIcon />
+                </IonButton>
+              </>
+            ) : (
+              <>
+              </>
+            )}
+          </IonToolbar>
+        </Grid>
+        <IonTabBar color="medium" style={{ border: "0" }}>
+          <IonTabButton id="home" tab="home" href="/dashboard">
+            <IonIcon icon={desktop} />
+            <IonLabel>Dashboard</IonLabel>
+          </IonTabButton>
+          <IonTabButton id="Search" tab="search" href="/search">
+            <IonIcon icon={searchCircle} />
+            <IonLabel>Search</IonLabel>
+          </IonTabButton>
+          <IonTabButton id="profile" tab="profile" href="/profile">
+            <IonIcon icon={personCircle} />
+            <IonLabel>Profile</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonHeader>
+      <IonContent color="light">
+        <Route path="/dashboard" component={App} exact={true} />
+        <Route path="/" render={() => <Redirect to="/dashboard" />} exact={true} />
+      </IonContent>
+    </IonPage>
+  </IonApp>
+</IonReactRouter>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<RenderApp />);
