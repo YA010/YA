0e76a7636787d757449d976e28bc91f8b2cd01f7
YA <yusufahmedbiz1@gmail.com>
2023-05-06 22:51:28 +0100

    test

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import {getFirestore} from "firebase/firestore"
 import { getAuth} from 'firebase/auth';
-let firebaseConfig = {
-  apiKey: "AIzaSyBGxzaBBh8bbciTD4PUC6k4_cgNCXcwrIo",
-  authDomain: "careerscamp-ai.firebaseapp.com",
-  projectId: "careerscamp-ai",
-  storageBucket: "careerscamp-ai.appspot.com",
-  messagingSenderId: "89974919951",
-  appId: "1:89974919951:web:e0416b2aab76c804a2e47a",
-  measurementId: "G-6G0EH2XG3P"
+const firebaseConfig = {
+  apiKey: "AIzaSyAp7RlikLGBk9Hec1_4Zn6fx4HdwDKPJLQ",
+  authDomain: "yainc-c172f.firebaseapp.com",
+  projectId: "yainc-c172f",
+  storageBucket: "yainc-c172f.appspot.com",
+  messagingSenderId: "326036975392",
+  appId: "1:326036975392:web:bde7e7c919ec7c7837f145",
+  measurementId: "G-7TQHE8Z18F"
 };
   
 
